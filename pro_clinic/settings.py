86f42356eef934de01f6b87c239e72a99786db8c
marylyne-mutindi <marylynemutindi@gmail.com>
2025-05-15 16:27:47 +0300

    changes

--- pro_clinic/settings.py
+++ pro_clinic/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clinic',  # Custom app for the clinic
 ]
 
 MIDDLEWARE = [
